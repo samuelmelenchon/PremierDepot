c92aaaf3ac548c02f07b2167cf1aa2246b7dbb78
Samuel MELENCHON <100763516+samuelmelenchon@users.noreply.github.com>
2024-03-11 14:23:58 +0100

    modification
    mise en place des fonctions division et multiplication

--- Operations.cpp
+++ Operations.cpp
@@ -17,3 +17,19 @@ int soustraction(int a, int b)
 	
 	return resultat;
 }
+int multplication(int a, int b)
+{
+	int resultat;
+	
+	resultat = a * b;
+	
+	return resultat;
+}
+float division(float a, float b)
+{
+	float resultat;
+	
+	resultat = a / b;
+	
+	return resultat;
+}
